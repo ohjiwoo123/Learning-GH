2bab91460a10811c54db14d9d47a651684c4ad66
JiwooOh <ohjiwoo123@naver.com>
2021-04-03 09:20:22 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by ohjiwoo on 2021/04/03.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
